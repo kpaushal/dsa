8794572c401f442d0a801f19087f8687efe2127e
Paushal kumar <157596504+kpaushal@users.noreply.github.com>
2026-06-30 23:01:28 +0530

    Time: 2 ms (55.95%), Space: 43.3 MB (20.64%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Solution {
+
+    public boolean backspaceCompare(String s, String t) {
+
+        return buildString(s).equals(buildString(t));
+    }
+
+    private String buildString(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+
+            if (ch == '#') {
+
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+
+            } else {
+
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+    }
+}
