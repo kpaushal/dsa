2a2c8e9aa6f8bea5de2ea0b881a5d0df6b2a0488
Paushal kumar <157596504+kpaushal@users.noreply.github.com>
2026-06-30 23:06:48 +0530

    Time: 1 ms (37.11%), Space: 43 MB (8.39%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    private Queue<Integer> queue;
+
+    public MyStack() {
+
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+
+        queue.offer(x);
+
+        int size = queue.size();
+
+        while (size > 1) {
+
+            queue.offer(queue.poll());
+            size--;
+        }
+    }
+
+    public int pop() {
+
+        return queue.poll();
+    }
+
+    public int top() {
+
+        return queue.peek();
+    }
+
+    public boolean empty() {
+
+        return queue.isEmpty();
+    }
+}
